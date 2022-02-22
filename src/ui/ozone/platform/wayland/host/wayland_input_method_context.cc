b7d556b464d564380b2aaca99c1d1074460b4f68
Eugeny Aristov <eugeny.aristov@lge.com>
2022-02-22 18:16:21 +0300

    [op][n_ups][ozone][vkb] Skip upstream text input handling logic on all platforms
    :Release Notes:
    Fixed broken VKB text deletion for PC backed by Google Ozone/Wayland via
    making upstream text input handling logic skip common for all platforms.
    
    :Detailed Notes:
    CL http://repo.lge.com:8080/c/5660 which intoduced the fix for webOS
    is extended for all platforms since related logic is shared
    within Neva appruntime (InputMethodNevaObserver) and isn't
    compatible with logic adjustments from recent upstream
    CL https://crrev.com/c/2878138.
    
    :Testing Performed:
    1. Build webruntime.
    2. Launch any app.
    3. Check the app is running and hasn't crash
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [WRN-17420] Port [vkb][g_ozone_wayland] Skip upstream text input
    handling logic on all platforms
    [NEVA-6716] [GOW][PC] Characters are not deleted with the backspace
    key in VKB
    
    Originally-Reviewed-on: http://repo.lge.com:8080/c/neva/chromium/+/5782
    Change-Id: Icbcc98123edb49c8697970577ba2c92a17951260
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium94/+/326849
    Reviewed-by: Commit Msg Checker <commit_msg@lge.com>
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: Alexander Kovalenko <alexander.kovalenko@lge.com>
    Reviewed-by: Ivan Murashov <ivan.murashov@lge.com>
    Reviewed-by: Vladislav Mukulov <vladislav.mukulov@lge.com>
    Reviewed-by: Donghyun Kim <donghyun11.kim@lge.com>
    Tested-by: Alexander Kovalenko <alexander.kovalenko@lge.com>
    Tested-by: Test BTS <test.bts@lge.com>
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium94/+/332949
    Tested-by: Donghyun Kim <donghyun11.kim@lge.com>

--- src/ui/ozone/platform/wayland/host/wayland_input_method_context.cc
+++ src/ui/ozone/platform/wayland/host/wayland_input_method_context.cc
@@ -351,9 +351,9 @@ void WaylandInputMethodContext::OnCommitString(base::StringPiece text) {
 
 void WaylandInputMethodContext::OnDeleteSurroundingText(int32_t index,
                                                         uint32_t length) {
-#if defined(OS_WEBOS)
+#if defined(USE_NEVA_APPRUNTIME)
   ime_delegate_->OnDeleteSurroundingText(index, length);
-#else   // defined(OS_WEBOS)
+#else   // defined(USE_NEVA_APPRUNTIME)
   // |index| and |length| are expected to be in UTF8 form, so we convert these
   // into UTF16 form.
   // Here, we use the surrounding text stored in SetSurroundingText which should
@@ -379,7 +379,7 @@ void WaylandInputMethodContext::OnDeleteSurroundingText(int32_t index,
   ime_delegate_->OnDeleteSurroundingText(
       offsets_for_adjustment[0],
       offsets_for_adjustment[1] - offsets_for_adjustment[0]);
-#endif  // defined(OS_WEBOS)
+#endif  // defined(USE_NEVA_APPRUNTIME)
 }
 
 void WaylandInputMethodContext::OnKeysym(uint32_t keysym,
