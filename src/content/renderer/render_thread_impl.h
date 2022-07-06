a4ebac98ce2025c0bbd06b9dbc258c9fabc74990
Pankaj Maharana <pankaj.maharana@lge.com>
2022-07-06 21:17:07 +0530

    Revert "[op][n_upsable][media] Add support for "enableWebOSVDA" property"
    :Release Notes:
    Revert the addition of suport for "enableWebOSVDA" appinfo property
    
    :Detailed Notes:
    This reverts commit 507ff3d456647219510885032316eab5628df738.
    
    Reason for revert: It is decided to enable VDA by default for OSE.
      So for other platforms --disable-web-media-player-neva will be
      used to enable VDA at runtime.
    
    :Testing Performed:
    Compilation test is done. Functionality is tested with changes to
    make VDA as defult instead of neva media player
    - https://gpro.lge.com/332586
    
    :QA Notes:
    Test-cases: QWO-49, QWO-74, QWO-1747, QWO-1753
    
    :Issues Addressed:
    [WRO-8271] Create chromium & wam patches for MCIL as default
    
    Change-Id: Ice2517e6d52de5cf8605529baff8289487d9926d
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium91/+/332056
    Reviewed-by: Karthick Sundaram <karthick.sundaram@lge.com>
    Reviewed-by: Donghyun Kim <donghyun11.kim@lge.com>
    Reviewed-by: Commit Msg Checker <commit_msg@lge.com>
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: Jose Dapena Paz <j.dapena@lgepartner.com>
    Reviewed-by: Pankaj Maharana <pankaj.maharana@lge.com>
    Tested-by: Pankaj Maharana <pankaj.maharana@lge.com>
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium94/+/333619
    Tested-by: Donghyun Kim <donghyun11.kim@lge.com>

--- src/content/renderer/render_thread_impl.h
+++ src/content/renderer/render_thread_impl.h
@@ -406,11 +406,6 @@ class CONTENT_EXPORT RenderThreadImpl
     video_frame_compositor_task_runner_ = task_runner;
   }
 
-#if defined(USE_NEVA_MEDIA)
-  void SetEnableWebOSVDA(bool enable);
-  bool GetEnableWebOSVDA() { return enable_webos_vda_; }
-#endif
-
  private:
   friend class RenderThreadImplBrowserTest;
   friend class AgentSchedulingGroup;
@@ -625,10 +620,6 @@ class CONTENT_EXPORT RenderThreadImpl
 
 #if defined(USE_NEVA_APPRUNTIME)
   unsigned suspension_count_ = 0;
-#endif
-
-#if defined(USE_NEVA_MEDIA)
-  bool enable_webos_vda_ = false;
 #endif
 
   // A mojo connection to the CompositingModeReporter service.
