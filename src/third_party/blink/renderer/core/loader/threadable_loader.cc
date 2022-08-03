5b3e2fb0d7c07c641a8209c1e56a546cd929a3b7
Wanchang Ryu <wanchang.ryu@lge.com>
2022-08-03 13:03:02 +0900

    fixup! [op][n_upsable][web_security] Adjust access to local resources
    :Release Notes:
    Removed filtering file access error for the async XMLHttpRequest.
    
    :Detailed Notes:
    Lower the condition of filtering file access error for the
    XMLHttpRequest to only for sync request because async request doesn't
    throw an exception. Async request needs to get error event listener
    called but it is not called because filtering file access error for both
    request type of sync and async.
    
    :Testing Performed:
    Launch mediadiscovery app.
    luna-send -n 1 -f luna://com.webos.applicationManager/launch '{"id":
    "com.webos.app.mediadiscovery"}'
    
    Run the JavaScript on the inspector console.
    1.
    const oReq = new XMLHttpRequest();
    oReq.addEventListener("load", transferComplete);
    oReq.addEventListener("error", transferFailed);
    oReq.addEventListener("abort", transferCanceled);
    oReq.open("GET", "file:///var/no_exist_file.html");
    oReq.send();
    function transferComplete(evt) {
      console.log("The transfer is complete.");
    }
    function transferFailed(evt) {
      console.log("An error occurred while transferring the file.");
    }
    function transferCanceled(evt) {
      console.log("The transfer has been canceled by the user.");
    }
    
    Expected: "An error occurred while transferring the file." should be printed on
    the inspector console.
    
    2.
    const oReq = new XMLHttpRequest();
    oReq.open("GET", "file:///var/no_exist_file.html", false);
    oReq.send();
    
    Expected: There should not be any JavaScript exception.
    
    :QA Notes:
    Test-cases: QWTC-7802
    
    :Issues Addressed:
    [WRO-9716] XMLHttpRequest - No error event occurs when requesting a file
    url that does not exist in the filesystem
    
    Change-Id: I3c91ebeb0e32e3b0ddc870119a8ca7f663cadfa6
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium94/+/334036
    Reviewed-by: Commit Msg Checker <commit_msg@lge.com>
    Reviewed-by: Wanchang Ryu <wanchang.ryu@lge.com>
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: Donghyun Kim <donghyun11.kim@lge.com>
    Tested-by: Wanchang Ryu <wanchang.ryu@lge.com>
    (cherry picked from commit e65f2a54b3985dadd41ba3c8154ae80dff3f6718)
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium94/+/334171
    Tested-by: Donghyun Kim <donghyun11.kim@lge.com>

--- src/third_party/blink/renderer/core/loader/threadable_loader.cc
+++ src/third_party/blink/renderer/core/loader/threadable_loader.cc
@@ -56,10 +56,6 @@
 #include "third_party/blink/renderer/platform/loader/fetch/resource_request.h"
 #include "third_party/blink/renderer/platform/wtf/assertions.h"
 
-#if defined(USE_NEVA_APPRUNTIME)
-#include "url/url_constants.h"
-#endif
-
 namespace blink {
 
 namespace {
@@ -349,12 +345,6 @@ void ThreadableLoader::NotifyFinished(Resource* resource) {
   checker_.NotifyFinished(resource);
 
   if (resource->ErrorOccurred()) {
-#if defined(USE_NEVA_APPRUNTIME)
-    // Don't throw an exception for loading of illegal local file. APPRUNTIME
-    // sets 'kIllegalDataURL' for local file request in case the file doesn't
-    // exist or the file loading is not allowed.
-    if (resource->Url() != url::kIllegalDataURL)
-#endif
     DispatchDidFail(resource->GetResourceError());
     return;
   }
